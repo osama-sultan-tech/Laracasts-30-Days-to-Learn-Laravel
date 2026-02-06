21589ae360b72c77da6e8e25f8ae2d8246814a04
Osama Sultan <80481444+osama-sultan-tech@users.noreply.github.com>
2026-02-06 21:47:40 +0300

    Day 22 - Make a Login and Registration System From Scratch: Part 2 (#12)
    * Day-12 Pivot Tables and BelongsToMany Relationships
    
    * Day-12 Homework
    
    * Day-13 Eager Loading and the N+1 Problem
    
    * Day-14 All You Need to Know About Pagination
    
    * Day-15 Understanding Database Seeders
    
    * Day-16 Forms and CSRF Explained (with Examples)
    
    * Day-17 Always Validate. Never Trust the User.
    
    * Day-18 Editing, Updating, and Deleting a Resource
    
    * Day-19 Routes Reloaded - 6 Essential Tips
    
    * Make a Login and Registration System From Scratch: Part 1
    
    * Day 22 - Make a Login and Registration System From Scratch: Part 2
    
    ---------
    
    Co-authored-by: Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,8 @@
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
-                        <img class="h-8 w-8" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                        <img class="h-8 w-8"
+                             src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
                              alt="Your Company">
                     </div>
                     <div class="hidden md:block">
@@ -34,8 +35,15 @@
                         @guest()
                             <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
                             <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
-
                         @endguest
+
+                        @auth()
+                            <form method="POST" action="/logout">
+                                @csrf
+                                <x-form-button>Log Out</x-form-button>
+
+                            </form>
+                        @endauth
 
 
                         <!-- Profile dropdown -->
@@ -106,9 +114,11 @@
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
 
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-            @if(!request()->is('jobs/create') )
-            <x-button href="/jobs/create">Create Job</x-button>
-            @endif
+            @auth()
+                @if(!request()->is('jobs/create') )
+                    <x-button href="/jobs/create">Create Job</x-button>
+                @endif
+            @endauth
         </div>
     </header>
 
