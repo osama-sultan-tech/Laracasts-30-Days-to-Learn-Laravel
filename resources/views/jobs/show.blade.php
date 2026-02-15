aabbfaeb58bab70fd9ef705128171edd76b20aa2
Osama Sultan <80481444+osama-sultan-tech@users.noreply.github.com>
2026-02-15 06:27:11 +0300

    Day 24 - How to Preview and Send Email Using Mailable Classes (#13)
    * Day-12 Pivot Tables and BelongsToMany Relationships
    
    * Day-12 Homework
    
    * Day-13 Eager Loading and the N+1 Problem
    
    * Day-14 All You Need to Know About Pagination
    
    * Day-15 Understanding Database Seeders
    
    * Day-16 Forms and CSRF Explained (with Examples)
    
    * Day-17 Always Validate. Never Trust the User.
    
    * Day-18 Editing, Updating, and Deleting a Resource
    
    * Day-19 Routes Reloaded - 6 Essential Tips
    
    * Make a Login and Registration System From Scratch: Part 1
    
    * Day 22 - Make a Login and Registration System From Scratch: Part 2
    
    * Day 23 - 6 Steps to Authorization Mastery
    
    * How to Preview and Send Email Using Mailable Classes
    
    ---------
    
    Co-authored-by: Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -9,5 +9,10 @@
         This job pays {{ $job['salary'] }} per year.
     </p>
 
-    <x-button href="/jobs/{{$job['id']}}/edit">Edit Job</x-button>
+    @can('edit', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        </p>
+    @endcan
+{{--    <x-button href="/jobs/{{$job['id']}}/edit">Edit Job</x-button>--}}
 </x-layout>
