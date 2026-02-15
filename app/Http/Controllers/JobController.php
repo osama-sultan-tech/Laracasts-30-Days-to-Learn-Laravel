299edafeb4ad11d39749eecfcd1d90046da31352
Osama Sultan <80481444+osama-sultan-tech@users.noreply.github.com>
2026-02-15 12:46:07 +0300

    Day 23 - 6 Steps to Authorization Mastery & Day 24 - How to Preview and Send Email Using Mailable Classes (#15)
    * Day-12 Pivot Tables and BelongsToMany Relationships
    
    * Day-12 Homework
    
    * Day-13 Eager Loading and the N+1 Problem
    
    * Day-14 All You Need to Know About Pagination
    
    * Day-15 Understanding Database Seeders
    
    * Day-16 Forms and CSRF Explained (with Examples)
    
    * Day-17 Always Validate. Never Trust the User.
    
    * Day-18 Editing, Updating, and Deleting a Resource
    
    * Day-19 Routes Reloaded - 6 Essential Tips
    
    * Make a Login and Registration System From Scratch: Part 1
    
    * Day 22 - Make a Login and Registration System From Scratch: Part 2
    
    * Day 23 - 6 Steps to Authorization Mastery
    
    * How to Preview and Send Email Using Mailable Classes
    
    ---------
    
    Co-authored-by: Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,8 +3,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -34,17 +38,24 @@ class JobController extends Controller
             'salary' => ['required']
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
+
+        Mail::to($job->employer->user)->send(
+            new JobPosted($job)
+        );
 
         return redirect('/jobs');
     }
 
     public function edit(Job $job)
     {
+
+        Gate::authorize('edit', $job);
+
         return view('jobs.edit', ['job' => $job]);
 
     }
