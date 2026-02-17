d1acaa31485bb52bdd2628f2f095d418b660f4b5
Osama Sultan <80481444+osama-sultan-tech@users.noreply.github.com>
2026-02-17 06:48:59 +0300

    Day 25 - Queues Are Easier Than You Think (#16)
    * Day-12 Pivot Tables and BelongsToMany Relationships
    
    * Day-12 Homework
    
    * Day-13 Eager Loading and the N+1 Problem
    
    * Day-14 All You Need to Know About Pagination
    
    * Day-15 Understanding Database Seeders
    
    * Day-16 Forms and CSRF Explained (with Examples)
    
    * Day-17 Always Validate. Never Trust the User.
    
    * Day-18 Editing, Updating, and Deleting a Resource
    
    * Day-19 Routes Reloaded - 6 Essential Tips
    
    * Make a Login and Registration System From Scratch: Part 1
    
    * Day 22 - Make a Login and Registration System From Scratch: Part 2
    
    * Day 23 - 6 Steps to Authorization Mastery
    
    * How to Preview and Send Email Using Mailable Classes
    
    * Queues Are Easier Than You Think
    
    ---------
    
    Co-authored-by: Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>

---
+++ app/Jobs/TranslateJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Job;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class TranslateJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Job $jobListing)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+//        AI::translateJob($this->jobListing->description,'Spanish');
+        logger('translating ' . $this->jobListing->title . ' to Spanish.');
+    }
+}
